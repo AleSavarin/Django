38d5c6498bf75461f82f53340d8d572320eb23fb
AleSavarin <savaing@hotmail.com>
2022-12-21 10:55:22 -0300

    BBDD inicio

--- Practica/Practica/settings.py
+++ Practica/Practica/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gestionPedidos', 
 ]
 
 MIDDLEWARE = [
@@ -74,14 +75,15 @@ WSGI_APPLICATION = 'Practica.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 '''
+# Completar los datos cuando los tenga
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', 
@@ -92,7 +94,7 @@ DATABASES = {
         'PORT': '5432', 
     } 
 }
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
