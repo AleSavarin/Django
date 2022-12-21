7252184b6aafbda9bd2457755c0a723da7bc32d7
AleSavarin <savaing@hotmail.com>
2022-12-21 11:50:15 -0300

    Panel Admin

--- Practica/gestionPedidos/models.py
+++ Practica/gestionPedidos/models.py
@@ -2,17 +2,19 @@ from django.db import models
 
 # Cada tabla de la BBDD se asocia a una clase.
 class Clientes(models.Model):
-    nombre = models.CharField(max_length=30)
-    direccion = models.CharField(max_length=50)
-    email = models.EmailField()
-    telefono = models.CharField(max_length=7)
+    nombre=models.CharField(max_length=30) 
+    direccion=models.CharField(max_length=50,verbose_name="La dirección") 
+    email=models.EmailField(blank=True,null=True)                          
+    telefono=models.CharField(max_length=7) 
+    def __str__(self):
+        return self.nombre
 
 class Articulos(models.Model):
     nombre = models.CharField(max_length=30)
     seccion = models.CharField(max_length=20)
     precio = models.IntegerField()
-    def __str__():
-        return 'El nombre es %s la sección %s y el precio %s'%(self.nombre,self.seccion,self.precio)
+    def __str__(self):
+        return 'El nombre es {} la sección {} y el precio {}'.format(self.nombre,self.seccion,self.precio)
 
 class Pedidos(models.Model):
     numero = models.IntegerField()
