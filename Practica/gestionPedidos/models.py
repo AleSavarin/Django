d3d6232d7329c1061cc485879ea8dfcb5bdc7473
AleSavarin <savaing@hotmail.com>
2022-12-21 11:15:20 -0300

    BBDD inicial 2

--- Practica/gestionPedidos/models.py
+++ Practica/gestionPedidos/models.py
@@ -11,6 +11,8 @@ class Articulos(models.Model):
     nombre = models.CharField(max_length=30)
     seccion = models.CharField(max_length=20)
     precio = models.IntegerField()
+    def __str__():
+        return 'El nombre es %s la sección %s y el precio %s'%(self.nombre,self.seccion,self.precio)
 
 class Pedidos(models.Model):
     numero = models.IntegerField()
